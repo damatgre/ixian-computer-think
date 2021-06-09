f50d3b9723aa20d263bbb2a250fd8db15212f2ff
damatgre <gregdamato2@gmail.com>
2021-06-09 19:20:01 -0400

    initial setup and pseudo

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,7 @@
 // TODO: Include packages needed for this application
+//require inquirer
+//require generateMarkdown
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -7,7 +10,11 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    //ask questions (activity 2 from monday)
+    //call generateMarkdown function which will return a string
+    //call writeToFile function pass to it a file name and the string returned by the generateMarkdown function
+}
 
 // Function call to initialize app
 init();
